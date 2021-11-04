fb0c6f0a5da9df8f73aec760a4b89d2bbad4dcf2
DanielPivnev <d.pivnev2003@gmail.com>
2021-11-04 13:06:17 +0100

    edit model 'Users'

--- ToDoList/users/models.py
+++ ToDoList/users/models.py
@@ -1,8 +1,9 @@
-from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
 
 
-class Users(AbstractUser):
+class Users(models.Model):
+    first_name = models.CharField(max_length=64)
+    last_name = models.CharField(max_length=64)
     email = models.EmailField(unique=True, blank=True)
