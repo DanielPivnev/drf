43da4262066e382aa1e3c7cc956734b3a6e99fe7
DanielPivnev <d.pivnev2003@gmail.com>
2021-11-04 13:27:24 +0100

    added router

--- ToDoList/users/views.py
+++ ToDoList/users/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ViewSet
+from .models import Users
+from .serializers import UserModelSerializer
 
-# Create your views here.
+
+class UserModelViewSet(ViewSet):
+    queryset = Users.objects.all()
+    serializer_class = UserModelSerializer
